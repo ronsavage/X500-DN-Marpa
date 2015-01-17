6879272ff1f93a0e39f34866d518def67ea89169
ronsavage <ron@savage.net.au>
2015-01-18 10:17:57 +1100

    Write docs for **::Actions. Fix scripts/pod2html.sh to match

--- scripts/pod2html.sh
+++ scripts/pod2html.sh
@@ -7,8 +7,9 @@ DIRS=$DIR1/$DIR2
 mkdir -p ~/savage.net.au/$DIRS/Marpa
 mkdir -p $DR/$DIRS/Marpa
 
-pod2html.pl -i lib/X500/DN/Marpa.pm     -o $DR/$DIRS/Marpa.html
-pod2html.pl -i lib/X500/DN/Marpa/DN.pm  -o $DR/$DIRS/Marpa/DN.html
-pod2html.pl -i lib/X500/DN/Marpa/RDN.pm -o $DR/$DIRS/Marpa/RDN.html
+pod2html.pl -i lib/X500/DN/Marpa.pm         -o $DR/$DIRS/Marpa.html
+pod2html.pl -i lib/X500/DN/Marpa/Actions.pm -o $DR/$DIRS/Marpa/Actions.html
+pod2html.pl -i lib/X500/DN/Marpa/DN.pm      -o $DR/$DIRS/Marpa/DN.html
+pod2html.pl -i lib/X500/DN/Marpa/RDN.pm     -o $DR/$DIRS/Marpa/RDN.html
 
 cp -r $DR/$DIRS/* ~/savage.net.au/$DIRS
